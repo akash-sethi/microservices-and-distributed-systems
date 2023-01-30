7db6b0f216de31387013b3cc251df3903993779e
akash sethi <akash.sethi@rabobank.nl>
2023-01-30 23:01:45 +0100

    updated customer table name

--- customer/src/main/kotlin/com/ms/customer/domain/Customer.kt
+++ customer/src/main/kotlin/com/ms/customer/domain/Customer.kt
@@ -4,9 +4,11 @@ import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.Table
 
+@Table(name = "customers")
 @Entity
-data class Customer(
+class Customer(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long?,
